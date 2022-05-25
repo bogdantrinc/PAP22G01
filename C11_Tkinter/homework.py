af4b436ba9d44bb17d7462462652363e37318355
Bogdan <bogdan.trinc@student.upt.ro>
2022-05-25 17:59:52 +0300

    ...

--- C11_Tkinter/homework.py
+++ C11_Tkinter/homework.py
@@ -2,6 +2,7 @@
     Create Mine Sweeper game layout with  5 rows and 5 columns
 """
 import tkinter
+from random import randint
 
 
 class MineSweeper():
@@ -13,16 +14,19 @@ class MineSweeper():
         for row in range(5):
             for col in range(5):
                 self.__setattr__(f'label{row, col}',
-                                 tkinter.Label(self.main_window, text=self.is_bomb(), width=10, height=2))
-                self.__setattr__(f'button{row, col}', tkinter.Button(self.main_window, text='O', width=10, height=5, command=self.do_nothing))
+                                 tkinter.Label(self.main_window, text=self.is_bomb(), width=10, height=5))
+                self.__setattr__(f'button{row, col}', tkinter.Button(self.main_window, text='O', width=10, height=5, command=self.push))
                 self.__getattribute__(f'label{row, col}').grid(row=row, column=col)
                 self.__getattribute__(f'button{row, col}').grid(row=row, column=col)
 
     def is_bomb(self):
-        return 1
+        return randint(0, 1)
 
-    def do_nothing(self):
-        pass
+    def push(self):
+        for row in range(5):
+            for col in range(5):
+                if self.__getattribute__(f'label{row, col}').cget("text") == 1:
+                    print(self.__getattribute__(f'label{row, col}').cget("text"))
 
     def run(self):
         self.main_window.mainloop()
