b322e61f6d1cbe7abfba9aabb4788043ee7bdbe4
Bogdan <bogdan.trinc@student.upt.ro>
2022-05-25 19:59:00 +0300

    ...

--- C12_Advanced Tkinter/homework.py
+++ C12_Advanced Tkinter/homework.py
@@ -3,7 +3,7 @@
 
     * Add menus to you Mine Sweeper game to be able to
     - Start new game
-    - Change game layout (5x5, 5x8, 8x8
+    - Change game layout (5x5, 5x8, 8x8)
     - Close game
 """
 import tkinter
