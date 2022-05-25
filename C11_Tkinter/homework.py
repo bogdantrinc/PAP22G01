05191b7c97827756602b45dd3f7dbfcbf9fa0e8c
Bogdan <bogdan.trinc@student.upt.ro>
2022-05-25 18:34:21 +0300

    ...

--- C11_Tkinter/homework.py
+++ C11_Tkinter/homework.py
@@ -15,18 +15,17 @@ class MineSweeper():
             for col in range(5):
                 self.__setattr__(f'label{row, col}',
                                  tkinter.Label(self.main_window, text=self.is_bomb(), width=10, height=5))
-                self.__setattr__(f'button{row, col}', tkinter.Button(self.main_window, text='O', width=10, height=5, command=self.push))
+                self.__setattr__(f'button{row, col}', tkinter.Button(self.main_window, width=10, height=5, command=lambda location=[row, col]: self.push(location)))
                 self.__getattribute__(f'label{row, col}').grid(row=row, column=col)
                 self.__getattribute__(f'button{row, col}').grid(row=row, column=col)
 
     def is_bomb(self):
         return randint(0, 1)
 
-    def push(self):
-        for row in range(5):
-            for col in range(5):
-                if self.__getattribute__(f'label{row, col}').cget("text") == 1:
-                    print(self.__getattribute__(f'label{row, col}').cget("text"))
+    def push(self, location: list, label=None):
+        if self.__getattribute__(f'label{location[0], location[1]}').cget("text") == 1:
+            print("Bomba!")
+        self.__getattribute__(f'button{location[0], location[1]}').destroy()
 
     def run(self):
         self.main_window.mainloop()
