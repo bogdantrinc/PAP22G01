dfd6a2fe40577fcec66d5a639e3d0cbcd3187b1d
Bogdan <bogdan.trinc@student.upt.ro>
2022-05-25 19:51:30 +0300

    ...

--- C12_Advanced Tkinter/homework.py
+++ C12_Advanced Tkinter/homework.py
@@ -13,12 +13,14 @@ from tkinter import messagebox
 
 class MineSweeper():
     title = "Minesweeper"
+    game_layout = {'5x5': [5, 5], '5x8': [5, 8], '8x8': [8, 8]}
 
-    def __init__(self):
+    def __init__(self, option: str='5x5'):
         self.main_window = tkinter.Tk()
         self.main_window.title(self.title)
-        for row in range(5):
-            for col in range(5):
+        self.add_menu()
+        for row in range(self.game_layout[option][0]):
+            for col in range(self.game_layout[option][1]):
                 self.__setattr__(f'label{row, col}',
                                  tkinter.Label(self.main_window, text=self.is_bomb(), width=10, height=5, bg='#C8C8C8'))
                 self.__setattr__(f'button{row, col}', tkinter.Button(self.main_window, width=10, height=5, command=lambda location=[row, col]: self.push(location)))
@@ -32,17 +34,47 @@ class MineSweeper():
         warning = None
         self.__getattribute__(f'button{location[0], location[1]}').destroy()
         if self.__getattribute__(f'label{location[0], location[1]}').cget("text") == 1:
-            print("Bomba!")
             warning = messagebox.askyesno(title=self.title, message="Game over! Try again?")
         if warning:
-            self.main_window.destroy()
             self.new_game()
+        elif warning is False:
+            self.main_window.destroy()
 
     def new_game(self):
+        self.main_window.destroy()
         new_game = MineSweeper()
         new_title = 'New ' + self.title
         new_game.main_window.title(new_title)
         new_game.run()
+
+    def layout_1(self):
+        self.main_window.destroy()
+        new_game = MineSweeper('5x8')
+        new_title = '5x8 ' + self.title
+        new_game.main_window.title(new_title)
+        new_game.run()
+
+    def layout_2(self):
+        self.main_window.destroy()
+        new_game = MineSweeper('8x8')
+        new_title = '8x8 ' + self.title
+        new_game.main_window.title(new_title)
+        new_game.run()
+
+    def add_menu(self):
+        main_layer = tkinter.Menu(self.main_window)
+        self.main_window.config(menu=main_layer)
+        layer_1 = tkinter.Menu(self.main_window)
+        main_layer.add_cascade(label='Options', menu=layer_1)
+        layer_1.add_command(label='New Game', command=self.new_game)
+        layer_2 = tkinter.Menu(self.main_window)
+        layer_1.add_cascade(label='Change layout', menu=layer_2)
+        layer_2.add_command(label='5x5', command=self.new_game)
+        layer_2.add_command(label='5x8', command=self.layout_1)
+        layer_2.add_command(label='8x8', command=self.layout_2)
+        layer_1.add_separator()
+        layer_1.add_command(label='Close', command=self.main_window.destroy)
+
 
     def run(self):
         self.main_window.mainloop()
